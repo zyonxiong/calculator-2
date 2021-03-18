f1ebc6f0ed877a631b9887ae2a17ad6b25c89a0b
Zyon Xiong <xiong.houa95@gmail.com>
2021-03-18 03:11:01 +0000

    Ended at working code and will add ifs for various operations tomorrow.

--- calculator.py
+++ calculator.py
@@ -4,4 +4,12 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+while True:
+    choice = input("Enter your equation >> ")
+    token = choice.split(' ')
+
+    if token[0] == 'q':
+        print("You will exit.")
+        break
+    else:
+        print("Will continue tomorrow.")
