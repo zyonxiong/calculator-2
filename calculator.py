dccc227759a8608cd4eb84863733ccbc997d5687
Zyon Xiong <xiong.houa95@gmail.com>
2021-03-18 14:58:46 +0000

    Created a new list for changed string char to be int char

--- calculator.py
+++ calculator.py
@@ -7,9 +7,21 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
     choice = input("Enter your equation >> ")
     token = choice.split(' ')
+    new_list = []
+
+    for index in token[1:]:
+        index = float(index)
+        new_list.append(index)
 
     if token[0] == 'q':
         print("You will exit.")
         break
+
+    
+    elif token[0] == "+":
+        result = new_list[0] + new_list[1]
+    elif token[0] == "-":
+        result = token[0] + token[1]
     else:
-        print("Will continue tomorrow.")
+            print("Not valid, try again.")
+    print(result)
